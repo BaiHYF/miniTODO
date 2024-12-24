be1d1a46169dce348c54f340c4b328daaed4d7de
baiheyufei <BaiHeYuFei@outlook.com>
2024-12-24 10:13:00 +0800

    feat(app): implement splash screen and update UI
    - Add SplashActivity to serve as the application entry point
    - Update AndroidManifest.xml to include new activities
    - Modify build.gradle.kts to include necessary dependencies
    - Update activity_splash.xml to include app title and logo
    - Update colors.xml to define new primary colors
    - Add AppTitle string resource

--- app/src/main/java/com/dev/mtodo/SplashActivity.java
+++ app/src/main/java/com/dev/mtodo/SplashActivity.java
@@ -1,12 +1,10 @@
 package com.dev.mtodo;
 
 import android.os.Bundle;
-
+import android.os.Handler;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -15,10 +13,11 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this , MainActivity.class));
+            finish();
+        }, 4000);   
+
     }
 }
